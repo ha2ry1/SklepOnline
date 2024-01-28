f5661d3d91f0981c07d5d1a6dba9ced5557682cc
JakubJ00 <154458649+JakubJ00@users.noreply.github.com>
2024-01-28 01:12:47 +0100

    Delete FrontEnd directory

--- FrontEnd/src/components/ShopItems.js
+++
@@ -1,9 +0,0 @@
-export default function ShopItems({ children }) {
-  return (
-    <section id="shop">
-      <h1>Dostępne produkty</h1>
-
-      <ul id="products">{children}</ul>
-    </section>
-  );
-}
